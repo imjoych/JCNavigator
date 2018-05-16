6b1ce5d8674396d307fe007ddca45fb7c65b5b2d
陈建军 <jianjun16@staff.weibo.com>
2018-05-16 12:17:58 +0800

    Update navigator demo.

--- JCNavigatorDemo/JCNavigatorDemo/AppDelegate.m
+++ JCNavigatorDemo/JCNavigatorDemo/AppDelegate.m
@@ -94,5 +94,9 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
+{
+    return [[JCNavigator sharedNavigator] openURL:url options:options];
+}
 
 @end
